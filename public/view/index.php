af78cb687116efca8021a3ab5f603f53bbe915e6
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2025-11-06 15:03:24 -0300

    Criando o index para ser depois uma lista de aulas

---
+++ public/view/index.php
@@ -0,0 +1,7 @@
+<?php
+    // Peço pra saber se conectou com a base de dados e mostrar na tela
+    require_once('../data/conn.php');
+
+    // Inserir o laço referente as aulas de PHP
+
+    // Essa parte faremos juntos, pessoal, mas antes, vamos olhar o conn.php
