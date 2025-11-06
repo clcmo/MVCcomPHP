26debc25cb757eea94ddc95a2c08ac787dadc5da
Milla <camila.leite.oliveira@gmail.com>
2025-11-06 18:04:43 -0300

    Criando minha rota

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <?php
+session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Autoload manual (ou use Composer)
+spl_autoload_register(function ($class) {
+    $class = str_replace('App\\', '', $class);
+    $class = str_replace('\\', '/', $class);
+    $file = __DIR__ . '/../app/' . $class . '.php';
+    
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 
+// Roteamento simples
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = trim($uri, '/');
 
-// Conexão com o banco
-require_once __DIR__ . '/data/conn.php';
-
-// Carrega a classe Aula
-require_once __DIR__ . '/Aula.php';
-
-// Instancia a classe
-$aulas = new Aula();
-
-// Lista todas as aulas
-echo $aulas->listarTodas();
+// Rotas
+if ($uri === '' || $uri === 'index.php') {
+    $controller = new App\Controllers\AulaController();
+    $controller->index();
+} elseif (preg_match('#^aula/(\d+)$#', $uri, $matches)) {
+    $controller = new App\Controllers\AulaController();
+    $controller->show($matches[1]);
+} elseif ($uri === 'comentario/adicionar' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $controller = new App\Controllers\AulaController();
+    $controller->addComment();
+} elseif ($uri === 'install.php') {
+    require_once 'install.php';
+} else {
+    http_response_code(404);
+    echo "404 - Página não encontrada";
+}
