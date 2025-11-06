99adfccbf11cdb45226b364eb614f8e6e60072df
Milla <camila.leite.oliveira@gmail.com>
2025-11-06 18:03:40 -0300

    Criando o Controller Base de interação

---
+++ controller/base.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controllers;
+
+// Classe de Controller irá fazer as interações com a View e com as Models
+class BaseController {
+    
+    protected function view($view, $data = []) {
+        extract($data);
+        
+        $viewFile = __DIR__ . '/../Views/' . $view . '.php';
+        
+        if (file_exists($viewFile)) {
+            require_once $viewFile;
+        } else {
+            die("View não encontrada: " . $view);
+        }
+    }
+    
+    protected function redirect($url) {
+        header("Location: " . $url);
+        exit;
+    }
+    
+    protected function json($data, $status = 200) {
+        http_response_code($status);
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
+}
